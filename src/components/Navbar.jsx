88a3ba79c8e4e381608ed864bc6f43213c505791
RLCHuncp <rlch06.uncp@gmail.com>
2020-08-05 22:27:47 -0500

    Componente de Barra de navegación

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from '../images/burger.png'
+import './styles/inicio.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="container__header">
+                <span>Burger</span><span>Queen</span>
+                 <img src={logo} alt="Logo"/>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
