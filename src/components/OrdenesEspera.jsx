da1b0d13a54823b47abcb7a0d8436f2f800bb5ed
jael-VRR <jrodriguezruidias@gmail.com>
2020-08-12 08:53:54 -0500

    orden en cocina

--- src/components/OrdenesEspera.jsx
+++ src/components/OrdenesEspera.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Navbar from './Navbar'
+import { firebase } from '../firebase';
+import { useState, useEffect } from 'react';
+
 
 const OrdenesEspera = () => {
+
+    const [detailsOrders, setDetailsOrders] = useState([])
+
+    useEffect(() => {
+        setDetailsOrders([]);
+        firebase
+            .firestore()
+            .collection('Orders')
+            .onSnapshot((snap) => {
+                const data = [];
+                snap.forEach((doc) => {
+                    data.push({
+                        id: doc.id,
+                        ...doc.data(),
+                    });
+                });
+                setDetailsOrders(data);
+            });
+
+    }, []);
+
+    console.log(detailsOrders)
+
     return (
-        <div>
+        <Fragment>
             <Navbar />
-            <p>
-                Ordenes en Espera
-            </p>
-        </div>
+            <div >
+                       <div className="card-order1"> 
+                            {detailsOrders.map((item) => (
+                                <ul key={item.id}>
+                                    <li>PEDIDO REALIZADO A LAS :{item.Fecha}</li>
+                                    <p  className = "box__title">CLIENTE:{item.client.client}</p>
+                                    <p>DETALLES PEDIDO:{item.Products.map((item)=>
+                                        <p>{item.Cantidad}  {item.Descripcion}  {item.Precio} </p>
+                                    )}</p>
+                             
+                                    <p>TOTAL:{item.Total}</p>
+                                </ul>
+                              
+                            ))
+                        }
+                        </div>
+                    </div>
+        </Fragment>
+
     )
+
 }
 
 export default OrdenesEspera
